a8003ef54b3f57ef40e7d6093addbd1cdf42a301
Michael Sarahan <msarahan@gmail.com>
2022-09-06 16:14:51 -0500

    set CC_ variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ sed -i.bak "s/@rust_arch@/$rust_arch/g" activate.sh
 sed -i.bak "s/@rust_arch_env@/$rust_arch_env/g" activate.sh
 sed -i.bak "s/@rust_default_cc@/$rust_default_cc/g" activate.sh
 sed -i.bak "s/@cross_target_platform@/$cross_target_platform/g" activate.sh
+sed -i.bak "s/@CONDA_RUST_HOST_LOWER@/$(echo $rust_arch_env_build | tr '[:upper:]' '[:lower:]')/g" activate.sh
 cp activate.sh ${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh
 
 if [[ "$cross_target_platform" == "win-64" ]]; then
